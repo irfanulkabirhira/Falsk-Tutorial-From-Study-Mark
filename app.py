6e5f98b4456c0d2463c73d59c5776378b4f66a22
MD Irfanul Kabir Hira <106048128+irfanulkabirhira@users.noreply.github.com>
2025-02-03 12:11:14 +0600

    Static Routing in Falsk

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = CEFlask(__name__)
+app = Flask(__name__)
 
-@app.route('/')
-
+@app.route('/')  # Home page
 def home():
-    return "Welcome to Flask"  # Fixed the typo in "Welcome"
+    return render_template('index.html')
 
-if __name__ == '__main__':  # Ensure proper indentation
+@app.route('/about')  # About page
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
     app.run(debug=True)
